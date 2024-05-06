2e23213c3e83a2d3eef06f68e90ddc2514eaf2b6
shawn174 <shawn.stephens@gmail.com>
2024-05-05 22:05:10 -0500

    Update prep.sh

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -23,14 +23,18 @@ brew install ansible
 echo "installing Ansible requirements"
 ansible-galaxy collection install geerlingguy.mac
 
+# Create ~/src
+mkdir ~/src
+cd ~/src
+
 # Grab the latest ansible-role-macos
 echo "Getting latest ansible-role-macos and unzipping"
 curl https://github.com/shawn174/ansible-role-macos/archive/refs/heads/main.zip -o ansible-role-macos.zip
 unzip ansible-role-macos.zip
 
 # Install my dotfiles for environment and change some settings with .osx
-echo "Getting dotfiles and unzipping"
-curl https://github.com/shawn174/dotfiles/archive/refs/heads/main.zip -o shawn174-dotfiles.zip
-unzip shawn174-dotfiles.zip
+#echo "Getting dotfiles and unzipping"
+#curl https://github.com/shawn174/dotfiles/archive/refs/heads/main.zip -o dotfiles.zip
+#unzip dotfiles.zip
 
-echo "Now run: ansible-playbook role.yml"
+echo "Now run: ansible-playbook ~/src/ansible-role-macos/role.yml"
